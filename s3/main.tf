e189668dc6851e771b2b2f7c221614d4e018e0de
Mohamad Amzar <mohdamzar96@gmail.com>
2023-09-15 02:01:16 +0800

    update var

--- s3/main.tf
+++ s3/main.tf
@@ -19,9 +19,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
+  access_key = env.aws_access_key
+  secret_key = env.aws_secret_key
+  region     = env.region
 }
 
 data "aws_iam_policy_document" "assume_role" {
